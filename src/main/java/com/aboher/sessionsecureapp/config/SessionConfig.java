400a13ea3abe867dc78310b8b8bf0116b51cda9a
Atilio Boher <atilioboher@gmail.com>
2024-11-22 19:39:50 -0300

    Remove Spring Docker Compose Support
    Remove Spring Docker Compose Support, now the redis container must be
    started manually executing docker compose up

--- src/main/java/com/aboher/sessionsecureapp/config/SessionConfig.java
+++ src/main/java/com/aboher/sessionsecureapp/config/SessionConfig.java
@@ -1,7 +1,9 @@
 package com.aboher.sessionsecureapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
 
@@ -9,8 +11,21 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisIndexedHttpSession
 public class SessionConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisHost);
+        redisConfig.setPort(redisPort);
+        redisConfig.setPassword(redisPassword);
+        return new LettuceConnectionFactory(redisConfig);
     }
 }
