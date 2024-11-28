59b0c3f30df1a849519e8bb1ee01c19c42d89619
Atilio Boher <atilioboher@gmail.com>
2024-11-28 10:46:02 -0300

    Fix bug related to sending emails
    For some reason the smtp server refuses to send emails if the from attribute
    is not set to the SMTP Server User, and although it was fixed previously,
    it was fixed only on one point of the code, now it's fixed and works
    everywhere on the code.

--- src/main/java/com/aboher/sessionsecureapp/service/EmailMessageSender.java
+++ src/main/java/com/aboher/sessionsecureapp/service/EmailMessageSender.java
@@ -1,6 +1,7 @@
 package com.aboher.sessionsecureapp.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -10,11 +11,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailMessageSender implements MessageSender<SimpleMailMessage> {
 
+    @Value("${spring.mail.username}")
+    private String smtpEmail;
+
     private final JavaMailSender javaMailSender;
 
     @Override
     @Async
     public void sendMessage(SimpleMailMessage message) {
+        message.setFrom(smtpEmail);
         javaMailSender.send(message);
     }
 }
